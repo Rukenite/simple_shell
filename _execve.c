a760b73aac5e1db5575c050d3b2f444f89862d52
rukenite <rukenite67@gmail.com>
2023-08-22 04:05:24 -0700

    _execve.c

--- _execve.c
+++ _execve.c
@@ -7,21 +7,21 @@
  * Return: 1 on error
  */
 int _execve(char **command, char *sh, int n)
-{
-        pid_t pid;
-        int i = 0, j = 0;
-        char *cmd[MAXSIZE];
+{	pid_t pid;
+	    int i = 0, j = 0;
+	    char *cmd[MAXSIZE];
 
-        pid = fork();
-        if (pid == 0)
-        {
-                if (execve(command[0], command, NULL))
-                {
-                dprintf(STDERR_FILENO, "%s: %d: %s: not found\n", sh, n, command[0]);
-                _free(command);
-                }
-        }
-        else
-                wait(NULL);
-        return (0);
+	    pid = fork()
+	if (pid == 0)
+	{
+		if (execve(command[0], command, NULL))
+	            {
+	            	dprintf(STDERR_FILENO, "%s: %d: %s: 
+					not found\n", sh, n, command[0]);
+	            	_free(command);
+	            }
+	    }
+	    else
+	            wait(NULL);
+	    return (0);
 }
