4a061602267460f67a943753045e874df645b35b
rukenite <rukenite67@gmail.com>
2023-08-23 09:02:09 -0700

    _execve.c

--- _execve.c
+++ _execve.c
@@ -6,16 +6,19 @@
  * @command: command to run
  * @sh : shell name
  * @n: line number
+ * @env: shell env variables.
  * Return: 0 on success
  */
-int _execve(char **command, char *sh, int n)
+int _execve(char **command, char *sh, int n, char **env)
 {
 	pid_t pid;
 
+	if (handle_builtins(command[0], env) == 0)
+		return (0);
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execve(command[0], command, NULL))
+		if (execve(command[0], command, env))
 			dprintf(STDERR_FILENO, "%s: %d: %s: not found\n", sh, n, command[0]);
 	}
 	else
